0262f0f5d8c5d51c741bd0144dbbeb5b4c068b1e
Jack <pengyulu92@gmail.com>
2026-04-15 13:38:50 +0100

    Add files via upload

---
+++ MouseLook.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+{
+    public class MouseLook : MonoBehaviour
+    {
+        [Header("Sensitivity setting")]
+        public float mouseXSensitivity = 100f;
+        public float mouseYSensitivity = 100f;
+
+        [Header("Vision limit")]
+        public float minY = -90f;
+        public float maxY = 90f;
+
+        public Transform playerBody;
+
+        [Header("Statu")]
+        public bool canLook = true;
+
+        private float xRotation = 0f;
+        private bool isCursorLocked = true;
+
+        void Start()
+        {
+            LockCursor();
+        }
+
+        void Update()
+        {
+            if (!canLook) return;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ToggleCursorLock();
+            }
+
+            if (isCursorLocked)
+            {
+                HandleMouseLook();
+            }
+        }
+
+        void HandleMouseLook()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseXSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseYSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, minY, maxY);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+
+        void LockCursor()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            isCursorLocked = true;
+        }
+
+        void UnlockCursor()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            isCursorLocked = false;
+        }
+
+        void ToggleCursorLock()
+        {
+            if (isCursorLocked)
+                UnlockCursor();
+            else
+                LockCursor();
+        }
+
+        public void SetCursorLock(bool locked)
+        {
+            if (locked)
+                LockCursor();
+            else
+                UnlockCursor();
+        }
+
+        public void DisableLook()
+        {
+            canLook = false;
+            UnlockCursor();
+        }
+
+        public void EnableLook()
+        {
+            canLook = true;
+            LockCursor();
+        }
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SojaExiles
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public CharacterController controller;
+
+        [Header("Movement setting")]
+        public float walkSpeed = 5f;
+        public float runSpeed = 8f;
+        public float gravity = -15f;
+
+        [Header("statu")]
+        public bool canMove = true;
+        public bool isRunning = false;
+
+        private Vector3 velocity;
+        private bool isGrounded;
+        private float currentSpeed;
+
+        void Update()
+        {
+            if (!canMove) return;
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            // Running
+            isRunning = Input.GetKey(KeyCode.LeftShift) && (x != 0 || z != 0);
+            currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+            controller.Move(move * currentSpeed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+
+        }
+
+        public void DisableMovement()
+        {
+            canMove = false;
+        }
+
+        public void EnableMovement()
+        {
+            canMove = true;
+        }
+    }
+}
